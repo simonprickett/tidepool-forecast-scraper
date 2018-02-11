68b54f53f25680502b515e100f695c81a4c70155
Simon Prickett <simon@crudworks.org>
2018-02-10 21:03:34 -0800

    Adds ajax load of backend data on page load, start of handlebars templates for success/error.

--- static/js/tidepool-forecast-scraper.js
+++ static/js/tidepool-forecast-scraper.js
@@ -1,1 +1,36 @@
 'use strict';
+
+var tidePoolApp = {
+    getTidePoolData: function() {
+        $.ajax({
+            method: 'GET',
+            url: '/api/tidepools',
+            success: function(data, status, xhr) {
+                var template;
+                var templateScript;
+
+                if (status === 'success' && typeof(data) === 'object') {
+                    template = $('#tidepools-template').html();
+                    templateScript = Handlebars.compile(template);
+
+                    $('#contentArea').html(templateScript(data));
+                } else {
+                    template = $('#error-template').html();
+                    templateScript = Handlebars.compile(template);
+
+                    $('#contentArea').html(templateScript());
+                }
+            },
+            error: function(xhr, status, error) {
+                var template = $('#error-template').html();
+                var templateScript = Handlebars.compile(template);
+
+                $('#contentArea').html(templateScript());
+            }
+        });
+    }
+};
+
+$(document).ready(function() {
+    tidePoolApp.getTidePoolData();
+});
